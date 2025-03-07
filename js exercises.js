10af3115c526a6434f534bfa5a44279e18438b04
uchimap <paulaiguaranuchima@gmail.com>
2025-03-07 16:02:07 +0000

    1 and 2 excercises

--- js exercises.js
+++ js exercises.js
@@ -1,1 +1,15 @@
 console.log("Welcome to excercise solutions file")
+
+//1-
+console.log ("Hello World")
+
+//2-
+function arithmetic (num1, num2){
+    let sum = num1+num2;
+    let difference = num1-num2;
+    let product = num1*num2;
+    let ratio = num1/numb2;
+    
+    console.log(sum, difference, product, ratio)
+}
+arithmetic(10,2)
